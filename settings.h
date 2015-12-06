ff30b9f4a12e7df01cd5c8924b94bee09795af5c
Aaron W. Tabor <aarontabor50@gmail.com>
2015-12-06 17:11:14 -0400

    Tweaked settings to allow room for one-million node graph.

--- settings.h
+++ settings.h
@@ -1,7 +1,7 @@
 #ifndef __settings_h__
 #define __settings_h__
 
-#define MAX_NODES 1000000
+#define MAX_NODES 1000001
 #define MAX_CHILDREN 11
-#define BUFFER_SIZE 2048
+#define BUFFER_SIZE 100000
 #endif
